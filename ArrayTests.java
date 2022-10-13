a0268b1f6316f97b3ad5dd4ff61e9ae0cfff525f
Glekko <kevinhuynhwong1492@gmail.com>
2022-10-13 15:25:11 -0700

    TempEdits

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,16 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3, 5, 10, 4, 2 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    
+    assertArrayEquals(new int[]{ 2, 4, 10, 5, 3}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1, 3, 5 };
+    assertArrayEquals(new int[]{5, 3, 1 }, ArrayExamples.reversed(input1));
   }
 }
